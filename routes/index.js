55b3df525c2f45db96d10b3ce93d99370e74d41a
“Ricky <grickyh@gmail.com>
2021-07-28 18:48:28 +0100

    mesage array

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Daisy",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Jody",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
